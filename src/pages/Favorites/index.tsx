97790faf50866df1aab5e9bb463a419ed2511fd5
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-06-24 18:13:11 -0300

    fix: modify favorites

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,13 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get('favorites');
+
+      const newFavorites = response.data.map(favorite => {
+        return { ...favorite, formattedPrice: formatValue(favorite.price) };
+      });
+
+      setFavorites([...newFavorites]);
     }
 
     loadFavorites();
